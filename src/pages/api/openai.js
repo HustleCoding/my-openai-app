91da654dd2daa71383a278a0d0e7c091f2a9cb88
HustleCoding <f.dobinciuc7@gmail.com>
2023-03-16 09:02:09 +0200

    Refactor openai api file in order to give us a nice format

--- src/pages/api/openai.js
+++ src/pages/api/openai.js
@@ -1,31 +1,48 @@
 import axios from "axios";
 
-export default async function handler(req, res) {
-  const prompt = req.body.prompt;
-  const apiKey = process.env.OPENAI_API_KEY;
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const { prompt } = req.body;
 
-  try {
-    const response = await axios.post(
-      "https://api.openai.com/v1/engines/davinci-codex/completions",
-      {
-        prompt,
-        max_tokens: 150,
-        n: 1,
-        stop: null,
-        temperature: 0.5,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${apiKey}`,
+    try {
+      const openaiApiKey = process.env.OPENAI_API_KEY;
+      const openaiApiUrl =
+        "https://api.openai.com/v1/engines/davinci-codex/completions";
+
+      const response = await axios.post(
+        openaiApiUrl,
+        {
+          prompt: `${prompt}\nAnswer:`,
+          max_tokens: 100,
+          n: 1,
+          stop: "\n",
+          temperature: 0.7,
         },
-      }
-    );
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${openaiApiKey}`,
+          },
+        }
+      );
 
-    const message = response.data.choices[0].text.trim();
-    res.status(200).json({ message });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to fetch data from OpenAI API" });
+      const answer = response.data.choices[0].text;
+
+      // Remove any unnecessary characters, such as brackets and commas, from the answer.
+      const cleanedAnswer = answer.replace(/[^a-zA-Z0-9\s\.\?]+/g, "").trim();
+
+      // Return the cleaned answer to the client.
+      res.status(200).json({ message: cleanedAnswer });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ message: "Error fetching response from OpenAI API" });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 }
+
+export default handler;
